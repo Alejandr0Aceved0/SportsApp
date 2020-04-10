55fb2c442482af8657b68263c067c97a5a0b90d5
Alejandr0Aceved0 <63383732+Alejandr0Aceved0@users.noreply.github.com>
2020-04-09 22:58:34 -0500

    Agrego Json e implementations, Librerias, Logeo y registro

--- app/src/main/java/com/example/sportsapp/Login.java
+++ app/src/main/java/com/example/sportsapp/Login.java
@@ -1,16 +1,241 @@
 package com.example.sportsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Pattern;
 
 public class Login extends AppCompatActivity {
 
+    private EditText mCorreo, mContraseña;
+    private Button mIngresar, mRegistrarse, Olvide_Contraseña;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private FirebaseUser User;
 
 
+    private Task<Void> mBaseDeDatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Verificación de internet
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+
+
+        } else {
+
+            //Si no hay internet
+
+            AlertDialog.Builder Sin_Internet = new AlertDialog.Builder(Login.this);
+            Sin_Internet.setTitle("Sin conexión");
+            Sin_Internet.setMessage("Revisa tu conexión a inernet e intenta \nnuevamente.");
+            Sin_Internet.setNegativeButton("Cerrar", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    System.exit(0);
+
+                }
+
+            });
+
+            Sin_Internet.show();
+
+        }
+        //Si hay internet
+
+        mAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser Usuario = FirebaseAuth.getInstance().getCurrentUser();
+
+            }
+        };
+
+
+        mCorreo= (EditText) findViewById(R.id.txt_field_Correo);
+        mContraseña=(EditText) findViewById(R.id.txt_field_Contraseña);
+        mIngresar=(Button) findViewById(R.id.btn_Ingresar);
+        mRegistrarse=(Button)findViewById(R.id.btn_Registrarse);
+        Olvide_Contraseña=(Button)findViewById(R.id.btn_OlvidoPass);
+
+        mRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Login.this, Registro.class);
+
+                startActivity(intent);
+
+                finish();
+
+                return;
+
+            }
+        });
+
+        mIngresar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                if(mCorreo.getText().toString().trim().equals("")) {
+
+                    mCorreo.setError("Por favor digite su correo.");
+                    mCorreo.requestFocus();
+
+                }else if(mContraseña.getText().toString().trim().equals("")){
+
+                    mContraseña.setError("Por favor digite su contraseña.");
+                    mContraseña.requestFocus();
+
+                } else if (!Validar_Email(mCorreo.getText().toString())){
+
+                    mCorreo.setError("Email no válido.");
+                    mCorreo.requestFocus();
+
+                }else if(mCorreo.getText().toString().trim().length() > 0 && mContraseña.getText().toString().trim().length()>0) {
+
+                    final String email = mCorreo.getText().toString();
+
+                    final String contrasena = mContraseña.getText().toString();
+
+                    mAuth.signInWithEmailAndPassword(email, contrasena).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (!task.isSuccessful()) {
+
+                                Toast.makeText(Login.this, "Error de ingreso.", Toast.LENGTH_SHORT).show();
+
+                            }else {
+
+                                //Dar valor al usuario actual
+
+                                User = mAuth.getCurrentUser();
+
+                                //Verifica si el usuario ya reviso su correo
+
+                                if(!User.isEmailVerified()){
+
+                                    //Aviso que no verifico
+
+                                    AlertDialog.Builder Auten = new AlertDialog.Builder(Login.this);
+
+                                    Auten.setTitle("Correo no verificado");
+                                    Auten.setMessage("Por favor verifica tu correo electrónico.");
+                                    Auten.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        }
+
+                                    });
+
+                                    Auten.show();
+
+                                }else{
+
+                                    //Si ya verifico el correo
+
+                                    mBaseDeDatos = FirebaseDatabase.getInstance().getReference().child("Usuarios").child("Administrador").child(User.getUid()).child("Contraseña").setValue(mContraseña.getText().toString().trim());
+
+                                    Intent intent = new Intent(Login.this, MenuPrincipal.class);
+                                    startActivity(intent);
+                                    finish();
+                                    return;
+
+                                }
+
+                            }
+
+                        }
+                    });
+
+                } else{
+
+                    Toast.makeText(Login.this,"Por favor llene los datos solicitados.",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+        });
+
+        Olvide_Contraseña.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Login.this, MenuPrincipal.class);
+
+                startActivity(intent);
+
+                finish();
+
+            }
+
+        });
+
+    }
+
+    private boolean Validar_Email(String email) {
+
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+
+        return pattern.matcher(email).matches();
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        mAuth.addAuthStateListener(firebaseAuthListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+
+        mAuth.addAuthStateListener(firebaseAuthListener);
+
     }
 }
--- app/src/main/java/com/example/sportsapp/Registro.java
+++ app/src/main/java/com/example/sportsapp/Registro.java
@@ -1,15 +1,253 @@
 package com.example.sportsapp;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Registro extends AppCompatActivity{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Registro extends AppCompatActivity {
+
+    private EditText mNombresClienteRegistro, mApellidosClienteRegistro, mEmailClienteRegistro, mContraseñaClienteRegistro, mContraseñaClienteRegistroV;
+    private Button mRegistroCliente, mCerrarRegistroCliente;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mBaseDeDatosCliente;
+    private FirebaseUser User;
+
+    private String idCliente;
+
+    private String mNombresClienteDato, mApellidosClienteDato, mEmailClienteDato, mContraseñaClienteDato, mContraseñaClienteDatoV;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        mNombresClienteRegistro = (EditText) findViewById(R.id.Text_Field_Nombres);
+        mApellidosClienteRegistro = (EditText) findViewById(R.id.Text_Field_Apellidos);
+        mEmailClienteRegistro = (EditText) findViewById(R.id.Text_Field_Correo);
+        mContraseñaClienteRegistro = (EditText) findViewById(R.id.Text_Field_Password);
+        mContraseñaClienteRegistroV = (EditText) findViewById(R.id.Text_Field_Password2);
+        mRegistroCliente = (Button) findViewById(R.id.BTN_Registrarse_usuario);
+        mCerrarRegistroCliente = (Button) findViewById(R.id.BTN_volver);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        AlertDialog.Builder Aviso = new AlertDialog.Builder(Registro.this);
+
+        Aviso.setTitle("Atencion!");
+        Aviso.setMessage("Por favor ingresa tus datos reales ya que despues no vas a poder actualizarlos.");
+        Aviso.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+
+        });
+
+        Aviso.show();
+
+
+        mRegistroCliente.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                if(Validacion()){
+
+                    if(mContraseñaClienteRegistro.getText().toString().trim().length() < 3){
+
+                        mContraseñaClienteRegistro.setError("Debe superar más de dos carácteres.");
+                        mContraseñaClienteRegistro.requestFocus();
+
+                    } else if(mNombresClienteRegistro.getText().toString().trim().length() < 3) {
+
+                        mNombresClienteRegistro.setError("Debe superar más de dos carácteres.");
+                        mNombresClienteRegistro.requestFocus();
+
+                    } else if(mApellidosClienteRegistro.getText().toString().trim().length() < 3){
+
+                        mApellidosClienteRegistro.setError("Debe superar más de dos carácteres.");
+                        mApellidosClienteRegistro.requestFocus();
+
+                    } else{
+
+                        final String email = mEmailClienteRegistro.getText().toString();
+                        final String contraseña = mContraseñaClienteRegistro.getText().toString();
+
+                        mAuth.createUserWithEmailAndPassword(email, contraseña).addOnCompleteListener(Registro.this, new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (!task.isSuccessful()) {
+
+                                    Toast.makeText(Registro.this, "Error de registro.", Toast.LENGTH_SHORT).show();
+
+                                } else {
+
+                                    idCliente = mAuth.getCurrentUser().getUid();
+                                    User = mAuth.getCurrentUser();
+                                    User.sendEmailVerification();
+                                    mBaseDeDatosCliente = FirebaseDatabase.getInstance().getReference().child("Usuarios").child("Administrador").child(idCliente);
+                                    mBaseDeDatosCliente.setValue(true);
+                                    GuardarInformacionDelCliente();
+
+                                }
+
+                            }
+
+                        });
+
+                    }
+
+                }
+
+            }
+
+        });
+        mCerrarRegistroCliente.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Registro.this, Login.class);
+
+                startActivity(intent);
+
+                finish();
+
+                return;
+
+            }
+
+        });
     }
+
+
+    private boolean Validacion(){
+
+        boolean Flag = false;
+
+        mNombresClienteDato = mNombresClienteRegistro.getText().toString().trim();
+        mApellidosClienteDato = mApellidosClienteRegistro.getText().toString().trim();
+        mEmailClienteDato = mEmailClienteRegistro.getText().toString().trim();
+        mContraseñaClienteDato = mContraseñaClienteRegistro.getText().toString().trim();
+        mContraseñaClienteDatoV = mContraseñaClienteRegistroV.getText().toString().trim();
+
+        if(mEmailClienteDato.equals("")){
+
+            mEmailClienteRegistro.setError("Por favor ingrese su correo.");
+            mEmailClienteRegistro.requestFocus();
+
+        }else if(mContraseñaClienteDato.equals("")){
+
+            mContraseñaClienteRegistro.setError("Por favor ingrese su contraseña.");
+            mContraseñaClienteRegistro.requestFocus();
+
+        }else if(!mContraseñaClienteDato.equals(mContraseñaClienteDatoV)){
+
+            mContraseñaClienteRegistroV.setError("Las contraseñas no coinciden.");
+            mContraseñaClienteRegistroV.requestFocus();
+
+        }else if(mNombresClienteDato.equals("")){
+
+            mNombresClienteRegistro.setError("Por favor ingrese su nombre(s).");
+            mNombresClienteRegistro.requestFocus();
+
+        }else if(mApellidosClienteDato.equals("")){
+
+            mApellidosClienteRegistro.setError("Por favor ingrese su apellido(s)");
+            mApellidosClienteRegistro.requestFocus();
+
+        }else if(!Validar_Email(mEmailClienteDato)){
+
+            mEmailClienteRegistro.setError("Email no válido.");
+            mEmailClienteRegistro.requestFocus();
+
+        }else{
+
+            Flag = true;
+
+        }
+
+        return Flag;
+
+    }
+
+    private boolean Validar_Email(String email) {
+
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+
+        return pattern.matcher(email).matches();
+
+    }
+
+    private void GuardarInformacionDelCliente(){
+
+        mNombresClienteDato = mNombresClienteRegistro.getText().toString();
+        mApellidosClienteDato = mApellidosClienteRegistro.getText().toString();
+        mEmailClienteDato = mEmailClienteRegistro.getText().toString();
+        mContraseñaClienteDato = mContraseñaClienteRegistro.getText().toString();
+
+
+        Map InformacionDelUsuario = new HashMap();
+        InformacionDelUsuario.put("Nombre", mNombresClienteDato.trim());
+        InformacionDelUsuario.put("Apellido", mApellidosClienteDato.trim());
+        InformacionDelUsuario.put("Email", mEmailClienteDato.trim());
+        InformacionDelUsuario.put("Contraseña", mContraseñaClienteDato.trim());
+        mBaseDeDatosCliente.updateChildren(InformacionDelUsuario);
+
+        AlertDialog.Builder Auten = new AlertDialog.Builder(Registro.this);
+
+        Auten.setTitle("Verificar correo electrónico");
+        Auten.setMessage("Por favor verifica tu correo para ser registrado satisfactoriamente.");
+        Auten.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent intent = new Intent(Registro.this, Login.class);
+
+                startActivity(intent);
+
+                finish();
+
+            }
+
+        });
+
+        Auten.show();
+
+    }
+
 }
